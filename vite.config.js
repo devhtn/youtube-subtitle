1469ebe75fd469f15cda45d3afbc510885fea94c
devhtn <devhtn@gmail.com>
2024-08-08 00:27:16 +0700

    init

---
+++ vite.config.js
@@ -0,0 +1,21 @@
+import path from 'path'
+import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+
+  resolve: {
+    //navigate the '~' sign to 'src' folder
+    //
+    alias: {
+      '~': path.resolve(
+        path.dirname(
+          new URL(import.meta.url).pathname.replace(/^\/([a-zA-Z]:\/)/, '$1'),
+        ),
+        'src',
+      ),
+    },
+  },
+})
